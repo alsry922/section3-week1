05e6b18a552d3067f12219394b8ff4a80c6302e5
mingyo <alsry922@gmail.com>
2022-10-21 03:32:56 +0900

    add coffeeController

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -1,9 +1,29 @@
 package com.codestates.coffee;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("v1/coffees")
+@RequestMapping(value = "v1/coffees", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class CoffeeController {
+  @PostMapping
+  public String postCoffee(@RequestParam("korName") String korName,
+                           @RequestParam("engName") String engName,
+                           @RequestParam("price") int price) {
+    String response = "{" +
+            "\"korName\":" + "\"" + korName + "\"," +
+            "\"engName\":" + "\"" + engName + "\"," +
+            "\"price\":" + "\"" + price + "\"" +
+            "}";
+    return response;
+  }
+  @GetMapping("/{coffee-id}")
+  public String getCoffee(@PathVariable("coffee-id") long coffeeId) {
+    return null;
+  }
+
+  @GetMapping
+  public String getCoffees() {
+    return null;
+  }
 }
